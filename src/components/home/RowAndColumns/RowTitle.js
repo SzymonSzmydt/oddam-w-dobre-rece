a89a3b713a7794f5582b27ee3e59884eb5ca7288
SzymonSzmydt <szymonszmydt@gmail.com>
2022-07-12 20:36:32 +0200

    ThreeColumns component created

---
+++ src/components/home/RowAndColumns/RowTitle.js
@@ -0,0 +1,11 @@
+import "./css/rowTitle.css";
+
+export function RowTitle() {
+
+    return (
+        <div className="threeColumns">
+            <p className="rowTitle"> Wystarczą 4 proste kroki </p>
+            <div className="rowTitle-img"/>
+        </div>
+    )
+}
